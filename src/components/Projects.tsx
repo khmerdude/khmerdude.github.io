b9dc31c8aa1a95dfd478142ba2bf4fce239a4586
khmerdude <jett1004@gmail.com>
2025-02-07 20:26:30 -0500

    Fix JSX parent element error in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,23 @@
-  <Image
-    src={'./images/etl-pipeline.jpg'}
-    alt={'ETL Data Pipeline Framework'}
-    objectFit={'cover'}
-  />
-  <Image
-    src={'./images/data-warehouse.jpg'}
-    alt={'Data Warehouse Migration'}
-    objectFit={'cover'}
-  />
-  <Image
-    src={'./images/technical-docs.jpg'}
-    alt={'Technical Documentation'}
-    objectFit={'cover'}
-  /> 
+import { Box } from '@chakra-ui/react'
+
+export default function Projects() {
+  return (
+    <Box>
+      <Image
+        src={'./images/etl-pipeline.jpg'}
+        alt={'ETL Data Pipeline Framework'}
+        objectFit={'cover'}
+      />
+      <Image
+        src={'./images/data-warehouse.jpg'}
+        alt={'Data Warehouse Migration'}
+        objectFit={'cover'}
+      />
+      <Image
+        src={'./images/technical-docs.jpg'}
+        alt={'Technical Documentation'}
+        objectFit={'cover'}
+      />
+    </Box>
+  )
+} 
